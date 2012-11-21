5c844f65ca3505f8257a65d18e87ee39e0a93d34
Heesang Chae <theand@gmail.com>
2012-11-21 14:06:08 +0900

    * 프로젝트 설정에서 Source 연결을 DZDocumentPicker로 수정.(앱코드에서는 폴더이름을 바로 바꾸기 때문에 XCode에서 해야했음)
    *sourcer.bundle을 프로젝트 폴더구조내로 가져옴.

---
+++ sourcer.bundle/build.py
@@ -0,0 +1,27 @@
+
+import cgi
+from xml.sax.saxutils import escape
+
+Maps = { \
+"index.html": {"lang":"HTML", "prism-type": "markup"}, \
+"normalize.css": {"lang":"CSS", "prism-type": "css"}, \
+"prism.js": {"lang":"JS", "prism-type": "javascript"}, \
+"CoreMatchers.java": {"lang":"Java", "prism-type" : "java"}, \
+"mac.sh": {"lang":"Bash", "prism-type" : "bash"}, \
+"core_proxy.clj": {"lang":"Clojure", "prism-type" : "clojure"}, \
+"class-wp-ajax-response.php": {"lang":"PHP", "prism-type" : "php"}, \
+"tokenizer.rb":  {"lang":"Ruby", "prism-type": "ruby"}, \
+\
+"php_stream_context.h":  {"lang":"C", "prism-type": "generic"}, \
+"util.py":  {"lang":"Python", "prism-type": "generic"}\
+}
+
+prefix = open("prefix.html", "r").read()
+postfix = open("postfix.html", "r").read()
+
+for src in Maps.keys():
+	file = open("sample_output/%s.html"%Maps[src]["lang"], "w+")
+	total = prefix%Maps[src]["prism-type"] + escape(open("sample_source/%s"%src).read() ) + postfix
+	file.write(total)
+	file.close()
+
---
+++ sourcer.bundle/js/main.js
@@ -0,0 +1,12 @@
+// $(function(){
+
+//  $("code").each(function(){
+//    $(this).html(function(index, html) {
+//      output = html.replace(/^(.*)$/mg, "<li><pre>$1</pre></li>");
+//    });
+//    $(this).replaceWith('<ol class="lncode">'+output+'</ol>');
+//  });
+
+//  });
+
+
